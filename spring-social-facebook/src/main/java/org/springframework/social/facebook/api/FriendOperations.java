f4e31a67fec11228aed545a29974a9687e588b88
Craig Walls <cwalls@vmware.com>
2011-11-14 15:17:06 -0600

    Adding paging (with offset and limit) to FriendOperations.getFriendProfiles(). SOCIALFB-44

--- spring-social-facebook/src/main/java/org/springframework/social/facebook/api/FriendOperations.java
+++ spring-social-facebook/src/main/java/org/springframework/social/facebook/api/FriendOperations.java
@@ -139,7 +139,9 @@ public interface FriendOperations {
 	List<String> getFriendIds();
 	
 	/**
-	 * Retrieves profile data for the authenticated user's friends.
+	 * Retrieves profile data for up to 100 of the authenticated user's friends.
+	 * For additional friend profiles, you must specify the offset and limit.
+	 * The list of profiles is ordered by each user's Facebook ID.
 	 * @return a list {@link FacebookProfile}s, each representing a friend of the user, or an empty list if not available.
 	 * @throws ApiException if there is an error while communicating with Facebook.
 	 * @throws MissingAuthorizationException if FacebookTemplate was not created with an access token.
@@ -147,7 +149,19 @@ public interface FriendOperations {
 	List<FacebookProfile> getFriendProfiles();
 
 	/**
+	 * Retrieves profile data for the authenticated user's friends.
+	 * The list of profiles is ordered by each user's Facebook ID.
+	 * @param offset the offset into the friends list to start retrieving profiles.
+	 * @param limit the maximum number of profiles to return.
+	 * @return a list {@link FacebookProfile}s, each representing a friend of the user, or an empty list if not available.
+	 * @throws ApiException if there is an error while communicating with Facebook.
+	 * @throws MissingAuthorizationException if FacebookTemplate was not created with an access token.
+	 */
+	List<FacebookProfile> getFriendProfiles(int offset, int limit);
+
+	/**
 	 * Retrieves a list of user references for the specified user's friends.
+	 * The list of profiles is ordered by each user's Facebook ID.
 	 * @param userId the user's ID
 	 * @return a list {@link Reference}s, each representing a friend of the user, or an empty list if not available.
 	 * @throws ApiException if there is an error while communicating with Facebook.
@@ -157,6 +171,7 @@ public interface FriendOperations {
 
 	/**
 	 * Retrieves a list of the authenticating user's friends' IDs.
+	 * The list of profiles is ordered by each user's Facebook ID.
 	 * @param userId the user's ID
 	 * @return a list of Strings, where each entry is the ID of one of the user's friends.
 	 * @throws ApiException if there is an error while communicating with Facebook.
@@ -165,11 +180,24 @@ public interface FriendOperations {
 	List<String> getFriendIds(String userId);
 	
 	/**
-	 * Retrieves profile data for the specified user's friends.
+	 * Retrieves profile data for up to 100 of the specified user's friends.
+	 * The list of profiles is ordered by each user's Facebook ID.
 	 * @param userId the user's ID
 	 * @return a list {@link FacebookProfile}s, each representing a friend of the user, or an empty list if not available.
 	 * @throws ApiException if there is an error while communicating with Facebook.
 	 * @throws MissingAuthorizationException if FacebookTemplate was not created with an access token.
 	 */
 	List<FacebookProfile> getFriendProfiles(String userId);
+
+	/**
+	 * Retrieves profile data for the specified user's friends.
+	 * The list of profiles is ordered by each user's Facebook ID.
+	 * @param userId the user's ID
+	 * @param offset the offset into the friends list to start retrieving profiles.
+	 * @param limit the maximum number of profiles to return.
+	 * @return a list {@link FacebookProfile}s, each representing a friend of the user, or an empty list if not available.
+	 * @throws ApiException if there is an error while communicating with Facebook.
+	 * @throws MissingAuthorizationException if FacebookTemplate was not created with an access token.
+	 */
+	List<FacebookProfile> getFriendProfiles(String userId, int offset, int limit);
 }
