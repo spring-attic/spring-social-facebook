84418c37c5cabbf91479645677ac7f10a1f9db0a
Craig Walls <cwalls@vmware.com>
2011-12-07 08:57:26 -0600

    Upgraded dependencies

--- spring-social-facebook/src/main/java/org/springframework/social/facebook/api/impl/json/PostMixin.java
+++ spring-social-facebook/src/main/java/org/springframework/social/facebook/api/impl/json/PostMixin.java
@@ -127,7 +127,7 @@ abstract class PostMixin {
 		@Override
 		public Integer deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
 			JsonNode tree = jp.readValueAsTree();
-			return tree.has("count") ? tree.get("count").getValueAsInt() : 0;
+			return tree.has("count") ? tree.get("count").getIntValue() : 0;
 		}
 	}
 }
