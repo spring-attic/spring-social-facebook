4cd2e5d5d913f4411fabb21d22260014fd81d52d
Craig Walls <cwalls@vmware.com>
2012-04-16 10:11:30 -0500

    Fix for NPE when no like information is given from Facebook. SOCIALFB-41

--- spring-social-facebook/src/test/java/org/springframework/social/facebook/api/FeedTemplateTest.java
+++ spring-social-facebook/src/test/java/org/springframework/social/facebook/api/FeedTemplateTest.java
@@ -349,12 +349,32 @@ public class FeedTemplateTest extends AbstractFacebookApiTest {
 		assertEquals("Hello world!", feedEntry.getMessage());
 		assertEquals("100001387295207", feedEntry.getFrom().getId());
 		assertEquals("Art Names", feedEntry.getFrom().getName());
-		assertEquals(2, feedEntry.getLikeCount());
+		assertEquals(2, (int) feedEntry.getLikeCount());
 		List<Reference> likes = feedEntry.getLikes();
 		assertEquals("1533260333", likes.get(0).getId());
 		assertEquals("Roy Clarkson", likes.get(0).getName());
 		assertEquals("1322692345", likes.get(1).getId());
 		assertEquals("Jim Smith", likes.get(1).getName());
+		assertEquals(2, feedEntry.getComments().size());
+		assertEquals(22, feedEntry.getCommentCount());
+		assertNull(feedEntry.getComments().get(1).getLikes());
+		assertEquals(3, feedEntry.getComments().get(1).getLikesCount());
+	}
+
+	@Test 
+	public void getFeedEntry_noLikes() {
+		mockServer.expect(requestTo("https://graph.facebook.com/100001387295207_123939024341978"))
+			.andExpect(method(GET))
+			.andExpect(header("Authorization", "OAuth someAccessToken"))
+			.andRespond(withResponse(jsonResource("testdata/post_nolikes"), responseHeaders));
+		Post feedEntry = facebook.feedOperations().getPost("100001387295207_123939024341978");
+		assertEquals(PostType.STATUS, feedEntry.getType());
+		assertEquals("100001387295207_123939024341978", feedEntry.getId());
+		assertEquals("Hello world!", feedEntry.getMessage());
+		assertEquals("100001387295207", feedEntry.getFrom().getId());
+		assertEquals("Art Names", feedEntry.getFrom().getName());
+		assertNull(feedEntry.getLikeCount());
+		assertNull(feedEntry.getLikes());
 		assertEquals(2, feedEntry.getComments().size());
 		assertEquals(22, feedEntry.getCommentCount());
 		assertNull(feedEntry.getComments().get(1).getLikes());
@@ -598,7 +618,7 @@ public class FeedTemplateTest extends AbstractFacebookApiTest {
 		assertEquals("When beetles fight these battles in a bottle with their paddles\nand the bottle's on a poodle and the poodle's eating noodles...\n...they call this a muddle puddle tweetle poodle beetle noodle\nbottle paddle battle.\"\n\u2014 Dr. Seuss (Fox in Socks)", posts.get(1).getMessage());
 		assertEquals(toDate("2011-05-13T01:41:50+0000"), posts.get(1).getCreatedTime());
 		assertEquals(toDate("2011-05-13T01:41:50+0000"), posts.get(1).getUpdatedTime());
-		assertEquals(12, posts.get(1).getLikeCount());
+		assertEquals(12, (int) posts.get(1).getLikeCount());
 		assertTrue(posts.get(2) instanceof StatusPost);
 		assertEquals("100000132946459_227565820591181", posts.get(2).getId());
 		assertEquals("100000132946459", posts.get(2).getFrom().getId());
