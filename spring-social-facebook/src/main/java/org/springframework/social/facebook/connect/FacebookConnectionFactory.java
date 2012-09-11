2c55eb318214b45dc8f3912f833165fc369690e5
Craig Walls <cwalls@vmware.com>
2012-09-11 16:25:12 -0500

    SOCIALFB-96: Add ability to configure a FacebookConnectionFactory with an application namespace so that Open Graph operations can be performed.

--- spring-social-facebook/src/main/java/org/springframework/social/facebook/connect/FacebookConnectionFactory.java
+++ spring-social-facebook/src/main/java/org/springframework/social/facebook/connect/FacebookConnectionFactory.java
@@ -21,11 +21,28 @@ import org.springframework.social.facebook.api.Facebook;
 /**
  * Facebook ConnectionFactory implementation.
  * @author Keith Donald
+ * @author Craig Walls
  */
 public class FacebookConnectionFactory extends OAuth2ConnectionFactory<Facebook> {
 
-	public FacebookConnectionFactory(String clientId, String clientSecret) {
-		super("facebook", new FacebookServiceProvider(clientId, clientSecret), new FacebookAdapter());
+	/**
+	 * Creates a FacebookConnectionFactory for the given application ID and secret.
+	 * Using this constructor, no application namespace is set (and therefore Facebook's Open Graph operations cannot be used).
+	 * @param appId The application's App ID as assigned by Facebook 
+	 * @param appSecret The application's App Secret as assigned by Facebook
+	 */
+	public FacebookConnectionFactory(String appId, String appSecret) {
+		this(appId, appSecret, null);
+	}
+
+	/**
+	 * Creates a FacebookConnectionFactory for the given application ID, secret, and namespace.
+	 * @param appId The application's App ID as assigned by Facebook 
+	 * @param appSecret The application's App Secret as assigned by Facebook
+	 * @param appNamespace The application's App Namespace as configured with Facebook. Enables use of Open Graph operations.
+	 */
+	public FacebookConnectionFactory(String appId, String appSecret, String appNamespace) {
+		super("facebook", new FacebookServiceProvider(appId, appSecret, appNamespace), new FacebookAdapter());
 	}
 
 }
