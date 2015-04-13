eade95f6b57e9782056193ed67b784532dec9dfc
pjkCochin <paul00091@yahoo.com>
2015-04-14 00:02:50 +0400

    issue-143

---
+++ spring-social-facebook/src/main/java/org/springframework/social/facebook/api/Place.java
@@ -0,0 +1,27 @@
+package org.springframework.social.facebook.api;
+
+public class Place {
+
+	private String id;
+	private Location location; 
+	private String name;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Location getLocation() {
+		return location;
+	}
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+}
