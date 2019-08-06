d11eac37f3a79f74ef7b65576ae49b73a463e232
Mihai Dediu <mihai@websi.ro>
2019-08-06 12:41:30 +0300

    Remove "context" from PROFILE_FIELDS array

--- spring-social-facebook/src/main/java/org/springframework/social/facebook/api/UserOperations.java
+++ spring-social-facebook/src/main/java/org/springframework/social/facebook/api/UserOperations.java
@@ -127,7 +127,7 @@ public interface UserOperations {
 	PagedList<Reference> search(String query);
 	
 	static final String[] PROFILE_FIELDS = {
-		"id", "about", "age_range", "birthday", "context", "cover", "currency", "devices", "education", "email", 
+		"id", "about", "age_range", "birthday", "cover", "currency", "devices", "education", "email", 
 		"favorite_athletes", "favorite_teams", "first_name", "gender", "hometown", "inspirational_people", "installed", "install_type",
 		"is_verified", "languages", "last_name", "link", "locale", "location", "meeting_for", "middle_name", "name", "name_format", 
 		"political", "quotes", "payment_pricepoints", "relationship_status", "religion", "security_settings", "significant_other", 
